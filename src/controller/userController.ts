5505c73ec4008f37a087a068f8f0fa2d08ac93ac
Mamun Shekh <smamun19@gmail.com>
2022-03-26 14:39:35 +0600

    feat: purge deepkit ORM

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,13 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import deepkit from "../db/deepkit";
-import { User } from "../db/deepkit";
 
 export const test = async (req: FastifyRequest, res: FastifyReply) => {
   try {
-    await deepkit.persist(new User("mamun"));
-    const allUsers = await deepkit.query(User).find();
-
-    return res.send(allUsers);
+    return res.send("test");
   } catch (error) {
     console.error(error);
   }
