a73ad806cc17b7f83f0604d55bd5a6d1b4cafed2
Mamun Shekh <smamun19@gmail.com>
2022-03-26 15:27:19 +0600

    feat: add prisma

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,15 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import prisma from "../db/prisma";
 
 export const test = async (req: FastifyRequest, res: FastifyReply) => {
   try {
-    return res.send("test");
+    await prisma.user.create({
+      data: { email: "asff@sfkajsf.com", name: "mamun", password: "randomone" },
+    });
+    const users = await prisma.user.findMany();
+    return res.send(users);
   } catch (error) {
     console.error(error);
+    return res.send("ERROR");
   }
 };
