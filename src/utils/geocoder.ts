9a0dddc465bdfe413f5cf3d98e5fbda5c535020e
S.M.Abdullah Al Mamun <smamun19@gmail.com>
2022-10-10 22:32:18 +0600

    Change geocoder provider.

--- src/utils/geocoder.ts
+++ src/utils/geocoder.ts
@@ -1,15 +1,11 @@
-import GeoCoder, { OpenStreetMapOptions } from "node-geocoder";
+import GeoCoder, { GenericOptions } from "node-geocoder";
 
-const options: OpenStreetMapOptions = {
-  provider: "openstreetmap",
+const options: GenericOptions = {
+  provider: "virtualearth",
+  apiKey: process.env.BING_API_KEY,
 };
 
 const geocoder = GeoCoder(options);
-
-// geocoder
-//   .reverse({ lat: 45.767, lon: 4.833 })
-//   .then((result) => console.log(result))
-//   .catch(console.log);
 
 export const getGeoAddress = async (lat: number, lon: number) => {
   const result = await geocoder.reverse({ lat, lon });
